0f9038d3c3883b4d6891e7c0646fac5c5c165da7
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-10-13 21:44:37 +0530

    Fix Modal outdated code

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -47,7 +47,7 @@ const Modal = ({ productId }: Props) => {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Dialog.Overlay className="fixed inset-0" />
+              <div className="fixed inset-0 bg-black bg-opacity-30" />
             </Transition.Child>
 
             <span
